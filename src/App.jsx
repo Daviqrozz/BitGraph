c70138d6f108ab21b1095b0742f8cf30faf060b2
DAVI RODRIGUES QUEIROZ <202302332218@alunos.estacio.br>
2024-03-07 21:35:30 -0300

    Grafico corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import Header from './components/cabeçalho/header'
 import Valor from './components/valor/valor'
 import Buttons from './components/botoes/index'
-import TradingViewWidget from './components/grafico/grafico'
+
 
 function App() {
   return (
     <div>
             <Header/>
             <Valor/>
-            <TradingViewWidget />
             <Buttons/>
 
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Header from './components/cabeçalho/header'
+import Valor from './components/valor/valor'
+import Buttons from './components/botoes/index'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
+  </React.StrictMode>,
+)
+ReactDOM.createRoot(document.getElementById('footer')).render(
+  <React.StrictMode>
+    <Valor />
+    <Buttons />
   </React.StrictMode>,
 )
