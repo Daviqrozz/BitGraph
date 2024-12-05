80f03d920465a45d0510d8d67569afdda4337243
davi <malzajax2@gmail.com>
2024-12-05 12:00:53 -0300

    Componente proprio para o grafico

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+
 import Header from './components/cabeçalho/header'
 import Valor from './components/valor/valor'
 import Buttons from './components/botoes/index'
+import TradingViewWidget from './components/graph/graph'
 
 
 function App() {
   return (
     <div>
             <Header/>
+            <TradingViewWidget />
             <Valor/>
             <Buttons/>
-
     </div>
   )
 }
--- src/components/botoes/index.jsx
+++ src/components/botoes/index.jsx
@@ -2,8 +2,8 @@ import './style.css'
 function Buttons(){
     return(
         <div className='button_box'>
-          <a className='buy' href="https://www.binance.com/pt/crypto/buy/USD/BTC"><button  className='buy'>Comprar</button></a>
-          <a className='sell' href="https://www.binance.com/pt/crypto/sell/USD/BTC"><button className='sell' >Vender</button></a>
+          <a className='buy' href="https://www.binance.com/pt/crypto/buy/USD/BTC" target="_blank"><button  className='buy'>Comprar</button></a>
+          <a className='sell' href="https://www.binance.com/pt/crypto/sell/USD/BTC" target="_blank"><button className='sell' >Vender</button></a>
         </div>
     )
 }
