248c2efde252965af6d5f6a3b95dce712f6e5519
daviqrozz <daviqrozz2@gmail.com>
2026-09-14 17:22:11 -0300

    chore:added docker structure

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Necessário para expor para fora do container Docker
+    port: 5173,
+    watch: {
+      usePolling: true, // Garante que o hot reload funcione em volumes no Windows
+    },
+  },
 })
